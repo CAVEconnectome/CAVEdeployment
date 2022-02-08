0aad458624e9c7e7b36c393b1bd7fdeeca5da37c
sdorkenw <sven.dorkenwald@googlemail.com>
2022-02-08 14:45:13 -0800

    addewd global env template

---
+++ environment_examples/global_env_template.sh
@@ -0,0 +1,137 @@
+export ENVIRONMENT={{ environment_name }}
+
+
+# GOOGLE ACCOUNT DETAILS
+
+export PROJECT_NAME={{ project_name }}
+export REGION={{ depl_region }}
+export ZONE={{ depl_zone }}
+export DNS_ZONE={{ dns_zone }}
+export DOMAIN_NAME={{ domain_name }}
+export CLUSTER_NAME=daf-$ENVIRONMENT
+export NETWORK_NAME=daf-$ENVIRONMENT-network
+export SUBNETWORK_NAME=daf-$ENVIRONMENT-network-sub
+export EXTERNAL_IP_ADDRESS_WO_QUOTES="$(gcloud compute addresses describe $CLUSTER_NAME --region=$REGION | grep address: | sed 's?address: ??')"
+export EXTERNAL_IP_ADDRESS="'${EXTERNAL_IP_ADDRESS_WO_QUOTES}'"
+export LETSENCRYPT_EMAIL={{ letsencrypt_email }}
+export LETSENCRYPT_SERVER=https://acme-v02.api.letsencrypt.org/directory
+export LETSENCRYPT_ISSUER_NAME=letsencrypt-staging
+
+# DATA PROJECT DETAILS
+
+export DOCKER_REPOSITORY={{ docker_repository }}
+
+
+# KUBERNETES DEPLOYMENT DETAILS
+
+export NGINX_INGRESS_CONTROLLER_NAME=contrasting-umbrellabird
+export NGINX_INGRESS_SERVICE_NAME=nginx-ingress-service
+export DNS_HOSTNAME=${ENVIRONMENT}.${DOMAIN_NAME}
+export CRD_GITHUB_PATH=https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
+export DNS_HOSTNAMES=({{ dns_hostnames }})
+export DNS_ZONES=({{ dns_zones }})
+
+DNS_HOSTNAMES_DASHED=''
+for dns_hostname in ${DNS_HOSTNAMES[@]};
+do
+    DNS_HOSTNAMES_DASHED=$DNS_HOSTNAMES_DASHED'
+  - '"${dns_hostname}"
+done
+export DNS_HOSTNAMES_DASHED
+export DNS_HOSTNAMES_DB_TABBED=$(echo "$DNS_HOSTNAMES_DASHED" | sed 's/  /    /g')
+export DNS_HOSTNAMES_TR_TABBED=$(echo "$DNS_HOSTNAMES_DASHED" | sed 's/  /      /g')
+
+DNS_INGRESS_PARA=''
+for dns_hostname in ${DNS_HOSTNAMES[@]};
+do
+    DNS_INGRESS_PARA=$DNS_INGRESS_PARA'
+  - host: '"${dns_hostname}"'
+    http: *http_rules'
+done
+export DNS_INGRESS_PARA
+
+
+# SQL INSTANCE
+
+export SQL_INSTANCE_CPU=1
+export SQL_INSTANCE_MEMORY=4
+export SQL_AUTH_DB_NAME=authentication
+export SQL_INFO_DB_NAME=infoservice
+export SQL_DB_TYPE=postgresql
+export POSTGRES_WRITE_USER=postgres
+export POSTGRES_WRITE_USER_PASSWORD={{ postgres_password }}
+
+# INSTANCE NAMES
+
+export REDIS_NAME=svenmd-daf-redis-$ENVIRONMENT
+export REDIS_IP="$(gcloud redis instances describe $REDIS_NAME --region=$REGION | sed -n -e 's/^host: \(.*\)$/\1/p')"
+export SQL_INSTANCE_NAME={{ sql_instance_name }}
+
+# SERVICE ACCOUNTS
+
+export NGLSTATE_SERVICE_ACCOUNT_NAME=daf-nglstate-$ENVIRONMENT
+export CLOUD_SQL_SERVICE_ACCOUNT_NAME=daf-cloudsqlproxy-$ENVIRONMENT
+export AUTH_SERVICE_ACCOUNT_NAME=daf-auth-$ENVIRONMENT
+export CLOUD_DNS_SERVICE_ACCOUNT_NAME=daf-clouddns-$ENVIRONMENT
+
+# SERVICE ACCOUNT SECRETS
+
+export NGLSTATE_SERVICE_ACCOUNT_SECRET=${NGLSTATE_SERVICE_ACCOUNT_NAME}-cred
+export INFOSERVICE_SERVICE_ACCOUNT_SECRET=daf-infoservice-secret-$ENVIRONMENT-cred
+export CLOUD_SQL_SERVICE_ACCOUNT_SECRET=${CLOUD_SQL_SERVICE_ACCOUNT_NAME}-cred
+export AUTH_SERVICE_ACCOUNT_SECRET=${AUTH_SERVICE_ACCOUNT_NAME}-cred
+export CLOUD_DNS_SERVICE_ACCOUNT_SECRET=${CLOUD_DNS_SERVICE_ACCOUNT_NAME}-cred
+export AUTH_OAUTH_SECRET=daf-oauth-${ENVIRONMENT}-cred
+export GOOGLE_SECRET_FILENAME=google-secret.json
+export OAUTH_SECRET_FILENAME=oauth-secret-${ENVIRONMENT}.json
+export CAVE_SECRET_FILENAME=cave-secret.json
+
+# PATHS
+
+export INFRASTRUCTURE_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export KEY_FOLDER_NAME=gcloud_keys_auth-$ENVIRONMENT
+export KEY_FOLDER=${INFRASTRUCTURE_FOLDER}/../${KEY_FOLDER_NAME}
+export YAML_FOLDER_NAME=kubeyamls-auth-${ENVIRONMENT}
+export YAML_FOLDER=${INFRASTRUCTURE_FOLDER}/../${YAML_FOLDER_NAME}
+
+# SERVICE VERSIONS
+
+export AUTH_VERSION=2.7.2
+export NGLSTATE_VERSION=refactor-39
+export INFOSERVICE_VERSION=3.8.1
+export EMANNOTATIONSCHEMAS_VERSION=3.5.3
+
+
+# REPLICAS
+
+export AUTH_REPLICAS=4
+export NGL_STATE_REPLICAS=1
+export EMANNOTATIONSCHEMAS_REPLICAS=1
+export INFO_REPLICAS=1
+
+
+# INFO
+
+export INFOSERVICE_CONFIG_VERSION=5
+export INFOSERVICE_CSRF_SECRET_KEY={{ infoservice_csrf_key }}
+export INFOSERVICE_SECRET_KEY={{ infoservice_secret_key }}
+export NEUROGLANCER_VIEWER_URL="https://neuromancer-seung-import.appspot.com/"
+
+
+# AUTH
+export AUTHSERVICE_CONFIG_VERSION=1.10
+export AUTHSERVICE_SECRET_KEY={{ authservice_secret_key }}
+
+export GLOBAL_SERVER=$DNS_HOSTNAME
+export AUTH_URI=${GLOBAL_SERVER}/auth
+export AUTH_URL=${GLOBAL_SERVER}/auth
+export INFO_URL=${GLOBAL_SERVER}/info
+export STICKY_AUTH_URL=${GLOBAL_SERVER}/sticky_auth
+
+
+# STATESERVER
+
+export JSON_DB_TABLE_NAME={{ ngl_link_db_table_name }}
+
+# AUTH-INFO
+export NGINX_CONFIG_VERSION=2
---
+++ environment_examples/render_global_env.py
@@ -0,0 +1,42 @@
+from jinja2 import Environment, FileSystemLoader
+
+def create_double_quoted_list_of_strings(l):
+    return ','.join([f'"{s}"' for s in l])
+
+def create_spaced_list_of_strings(l):
+    return ' '.join([f'"{s}"' for s in l])
+
+var_dict = {
+    "environment_name": "global_depl",
+    "project_name": "my_project",
+    "pcg_bucket_name": "pcg_bucket",
+    "depl_region": "sweet-sweet-kingdom",
+    "depl_zone": "sweet-sweet-kingdom-a",
+    "dns_zone": "kingdom",
+    "domain_name": "domain",
+    "letsencrypt_email": "my_email",
+    "docker_repository": "docker.io/caveconnectome",
+    "add_dns_hostnames": ["add_hostname1", "add_hostname2"],
+    "add_dns_zones": ["$DNS_ZONE", "add_zone"],
+    "postgres_password": "my_sweet_secret",
+    "sql_instance_name": "daf-global-depl",
+    "add_storage_secrets": ["my-secret-secret.json", "my-secret-secret2.json"] ,
+    "global_server": "global.my-dns.com",
+    "infoservice_csrf_key": "random_key",
+    "infoservice_secret_key": "random_key",
+    "authservice_secret_key": "random_key",
+    "ngl_link_db_table_name": "ngl_link_db",
+}
+
+# Additional modifications to parameters and checks
+var_dict["dns_hostnames"] = create_spaced_list_of_strings(["$DNS_HOSTNAME"] + var_dict["add_dns_hostnames"])
+var_dict["dns_zones"] = create_spaced_list_of_strings(["$DNS_ZONES"] + var_dict["add_dns_hostnames"])
+
+# Load and render template
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('global_env_template.sh')
+rendered_template = template.render(var_dict)
+
+# Write rendered tempalte
+with open(f"{var_dict['environment_name']}.sh", "w") as f:
+    f.write(rendered_template)
