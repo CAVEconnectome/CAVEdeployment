868438b578e8867f57f2de8f9cdd55f809488e7a
sdorkenw <sven.dorkenwald@googlemail.com>
2022-05-05 17:45:14 -0400

    fixing external SQL access

--- environment_examples/local_env_template.sh
+++ environment_examples/local_env_template.sh
@@ -25,8 +25,8 @@ export SUPPORTED_DATASTACKS='{{ supported_datastacks }}'
 export DATA_PROJECT_NAME={{ data_project_name }}
 export DATA_REGION={{ data_project_region }}
 export SQL_PROJECT_NAME={{ sql_project_name }}
-export SQL_REGION={{ SQL_REGION }}
-export SQL_ZONE={{ sql_project_zone }}
+export SQL_REGION={{ sql_region }}
+export SQL_ZONE={{ sql_zone }}
 export DOCKER_REPOSITORY={{ docker_repository }}
 
 # KUBERNETES DEPLOYMENT DETAILS
