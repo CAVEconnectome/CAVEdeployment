365dc747fe6cc50b893f6331c1b83f7168857a5d
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2023-03-21 10:34:50 -0500

    added deployment script for pcg redis

---
+++ infrastructure/local/deploy_pcg_redis.sh
@@ -0,0 +1,4 @@
+source env_config.sh
+source $ENV_REPO_PATH/$1.sh
+
+gcloud redis instances create $PCG_REDIS_NAME --size=1 --region=$REGION --zone=$ZONE --network=$NETWORK_NAME --redis-config maxmemory-policy=allkeys-lru
