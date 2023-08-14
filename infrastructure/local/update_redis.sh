d69c11812c16b3269f547522ca62b44f036aa48c
Forrest Collman <forrest.collman@gmail.com>
2023-08-14 12:06:44 -0700

    upgraded redis helm version

--- infrastructure/local/update_redis.sh
+++ infrastructure/local/update_redis.sh
@@ -4,4 +4,4 @@ source $ENV_REPO_PATH/$1.sh
 ./infrastructure/local/switch_context.sh $1
 ./infrastructure/local/create_yaml_files_from_templates.sh $1
 
-helm upgrade --recreate-pods -f ${YAML_FOLDER}/redis_production_values.yml redis-release bitnami/redis  --version 10.7.11
+helm  upgrade -f ${YAML_FOLDER}/redis_production_values.yml redis-release bitnami/redis --version 17.11.2 
