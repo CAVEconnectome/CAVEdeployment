9d6309483c16cf734a23b36347564012a0699820
jinhan <choi.jinhan@gmail.com>
2024-05-07 23:05:25 -0400

    add materialization pub/sub workflow

--- infrastructure/local/configure_pubsub.sh
+++ infrastructure/local/configure_pubsub.sh
@@ -6,6 +6,9 @@ source $ENV_REPO_PATH/$1.sh
 gcloud pubsub topics create ${L2CACHE_EXCHANGE}
 gcloud pubsub subscriptions create ${L2CACHE_UPDATE_QUEUE} --topic=${L2CACHE_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=60 --expiration-period="never" --max-retry-delay=10
 
+gcloud pubsub topics create ${MAT_TASKQUEUE_TOPIC}
+gcloud pubsub subscriptions create ${MAT_TASKQUEUE_SUBSCRIPTION} --topic=${MAT_TASKQUEUE_TOPIC} --topic-project=${PROJECT_NAME} --ack-deadline=300 --expiration-period="never" --max-retry-delay=10
+
 gcloud pubsub topics create ${PYCHUNKEDGRAPH_EDITS_EXCHANGE}
 gcloud pubsub subscriptions create ${PYCHUNKEDGRAPH_REMESH_QUEUE} --topic=${PYCHUNKEDGRAPH_EDITS_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=600 --expiration-period="never" --max-retry-delay=10 --message-filter='attributes.remesh_priority="true"'
 gcloud pubsub subscriptions create ${PYCHUNKEDGRAPH_LOW_PRIORITY_REMESH_QUEUE} --topic=${PYCHUNKEDGRAPH_EDITS_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=600 --expiration-period="never" --max-retry-delay=10 --message-filter='attributes.remesh_priority="false"' 
