11c1c4747a9a84e15b82715e7942c6a9e193335a
Keith Wiley <keith.wiley@alleninstitute.org>
2024-07-17 15:31:02 -0700

    Skeleton cache tuning

--- env_config.sh
+++ env_config.sh
@@ -1,3 +1,3 @@
 # Set the path to your private environments repo
-export ENV_REPO_PATH="/Users/svenmd/PrinceAllenCAVE/"
+export ENV_REPO_PATH="/Users/keith.wiley/Work/Code/PrinceAllenCAVE"
 # export ENV_REPO_PATH="/Users/svenmd/h01-cave-secret/"
