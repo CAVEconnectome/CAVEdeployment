b28fbcad817ab715071e62cde3da44ee05d2f138
Keith Wiley <keith.wiley@alleninstitute.org>
2025-02-12 12:40:55 -0800

    Skeletonization low/high priority pubsub queues

--- infrastructure/local/configure_pubsub.sh
+++ infrastructure/local/configure_pubsub.sh
@@ -6,8 +6,11 @@ source $ENV_REPO_PATH/$1.sh
 gcloud pubsub topics create ${L2CACHE_EXCHANGE}
 gcloud pubsub subscriptions create ${L2CACHE_UPDATE_QUEUE} --topic=${L2CACHE_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=60 --expiration-period="never" --max-retry-delay=10
 
-gcloud pubsub topics create ${SKELETON_CACHE_EXCHANGE}
-gcloud pubsub subscriptions create ${SKELETON_CACHE_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10
+gcloud pubsub topics create ${SKELETON_CACHE_LOW_PRIORITY_EXCHANGE}
+gcloud pubsub subscriptions create ${SKELETON_CACHE_LOW_PRIORITY_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_LOW_PRIORITY_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10
+
+gcloud pubsub topics create ${SKELETON_CACHE_HIGH_PRIORITY_EXCHANGE}
+gcloud pubsub subscriptions create ${SKELETON_CACHE_HIGH_PRIORITY_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_HIGH_PRIORITY_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10
 
 gcloud pubsub topics create ${PYCHUNKEDGRAPH_EDITS_EXCHANGE}
 gcloud pubsub subscriptions create ${PYCHUNKEDGRAPH_REMESH_QUEUE} --topic=${PYCHUNKEDGRAPH_EDITS_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=600 --expiration-period="never" --max-retry-delay=10 --message-filter='attributes.remesh_priority="true"'
