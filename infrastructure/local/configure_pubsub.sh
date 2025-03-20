bf76fad955499a874f135c9e3d58857c59811c85
Keith Wiley <keith.wiley@alleninstitute.org>
2025-03-20 16:31:33 -0700

    SkeletonCache changes

--- infrastructure/local/configure_pubsub.sh
+++ infrastructure/local/configure_pubsub.sh
@@ -7,11 +7,14 @@ source ./infrastructure/local/convert_variables.sh
 gcloud pubsub topics create ${L2CACHE_EXCHANGE}
 gcloud pubsub subscriptions create ${L2CACHE_UPDATE_QUEUE} --topic=${L2CACHE_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=60 --expiration-period="never" --max-retry-delay=10
 
+gcloud pubsub topics create ${SKELETON_CACHE_DEAD_LETTER_EXCHANGE}
+gcloud pubsub subscriptions create ${SKELETON_CACHE_DEAD_LETTER_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_DEAD_LETTER_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10
+
 gcloud pubsub topics create ${SKELETON_CACHE_LOW_PRIORITY_EXCHANGE}
-gcloud pubsub subscriptions create ${SKELETON_CACHE_LOW_PRIORITY_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_LOW_PRIORITY_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10
+gcloud pubsub subscriptions create ${SKELETON_CACHE_LOW_PRIORITY_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_LOW_PRIORITY_EXCHANGE} --dead-letter-topic=${SKELETON_CACHE_DEAD_LETTER_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10 --max-delivery-attempts=5
 
 gcloud pubsub topics create ${SKELETON_CACHE_HIGH_PRIORITY_EXCHANGE}
-gcloud pubsub subscriptions create ${SKELETON_CACHE_HIGH_PRIORITY_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_HIGH_PRIORITY_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10
+gcloud pubsub subscriptions create ${SKELETON_CACHE_HIGH_PRIORITY_RETRIEVE_QUEUE} --topic=${SKELETON_CACHE_HIGH_PRIORITY_EXCHANGE} --dead-letter-topic=${SKELETON_CACHE_DEAD_LETTER_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=180 --expiration-period="never" --max-retry-delay=10 --max-delivery-attempts=5
 
 gcloud pubsub topics create ${PYCHUNKEDGRAPH_EDITS_EXCHANGE}
 gcloud pubsub subscriptions create ${PYCHUNKEDGRAPH_REMESH_QUEUE} --topic=${PYCHUNKEDGRAPH_EDITS_EXCHANGE} --topic-project=${PROJECT_NAME} --ack-deadline=600 --expiration-period="never" --max-retry-delay=10 --message-filter='attributes.remesh_priority="true"'
